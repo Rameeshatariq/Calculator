490cab4ea27f806b32e6fb9d6c6fa8a180050c00
Rameeshatariq <35740845+Rameeshatariq@users.noreply.github.com>
2021-10-02 15:46:25 +0500

    [UI Added]

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View) {
+        tvInput.append((view as Button).text)
+
+        //Toast.makeText(this,"Works", Toast.LENGTH_SHORT).show()
+    }
+
+    fun onClear(view : View){
+        tvInput.setText("")
+    }
+}
